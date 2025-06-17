68498476da44cc1178fd3eae479865547ef18450
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-06-17 17:36:15 +0500

    feat:Adding InputFiled as a reusable component

---
+++ src/components/ui/InputField.jsx
@@ -0,0 +1,37 @@
+// components/commons/InputField.jsx
+import React from "react";
+
+const InputField = ({
+  label,
+  name,
+  value,
+  onChange,
+  placeholder,
+  type = "text",
+  required = false,
+  className = "",
+  ...rest
+}) => {
+  return (
+    <div className={`flex flex-col gap-1 `}>
+      {label && (
+        <label htmlFor={name} className="text-sm font-medium text-gray-700">
+          {label}
+          {required && <span className="text-red-500"> *</span>}
+        </label>
+      )}
+      <input
+        id={name}
+        name={name}
+        type={type}
+        value={value}
+        onChange={onChange}
+        className={`!p-4 ${className}`}
+        placeholder={placeholder}
+        {...rest}
+      />
+    </div>
+  );
+};
+
+export default InputField;
