8044ceb869f58e31513e27836e3a73465d725c6c
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-06-17 17:48:10 +0500

    feat:Adding padding to content

--- src/app/admin/dashboard/page.js
+++ src/app/admin/dashboard/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 const AdminDashobard = () => {
   return (
-    <div className='text-black mt-26 h-screen'>
+    <div className='text-black h-screen pt-[4.5rem]'>
         <h1>AdminDashobard</h1>
     </div>
   )
