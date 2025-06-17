b94977bb22370c762d6d43b1f4b409ff8f44c6d4
rahmatshah1310 <rahmatshah1310@gmail.com>
2025-06-17 17:34:34 +0500

    feat:Adding admin dashobard page in app

---
+++ src/app/admin/dashboard/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AdminDashobard = () => {
+  return (
+    <div className='text-black mt-26 h-screen'>
+        <h1>AdminDashobard</h1>
+    </div>
+  )
+}
+
+export default AdminDashobard
